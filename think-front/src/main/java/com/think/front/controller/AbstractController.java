74d461f6fdb7d96f1aa442e4d0efa6f23dc06287
qiudong <qiu_dong@suixingpay.com>
2017-12-20 20:23:24 +0800

    热部署

---
+++ think-front/src/main/java/com/think/front/controller/AbstractController.java
@@ -0,0 +1,16 @@
+package com.think.front.controller;
+
+/**
+ * Created by qiudong on 2017/12/20.
+ */
+
+public class AbstractController {
+
+    /**
+     * 获取用户Token
+     */
+    private String getToken(){
+
+        return "";
+    }
+}
--- think-front/src/main/java/com/think/front/controller/demo/DemoController.java
+++ think-front/src/main/java/com/think/front/controller/demo/DemoController.java
@@ -1,5 +1,6 @@
-package com.think.front.controller;
+package com.think.front.controller.demo;
 
+import com.think.front.controller.AbstractController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,9 +9,9 @@ import org.springframework.web.bind.annotation.RestController;
  * Created by qiudong on 2017/12/20.
  */
 @RestController
-public class DemoController {
+public class DemoController extends AbstractController{
 
-    @RequestMapping("/greeting")
+    @RequestMapping("/greeting1")
     public String greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
         return name + "hello";
     }
